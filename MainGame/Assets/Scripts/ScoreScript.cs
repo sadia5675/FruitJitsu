646b9f76c9abcf42c7340c8d9b053d96779bbe88
Huong Ly Nguyen <huongly.nguyen@student.hs-rm.de>
2024-01-06 22:50:21 +0100

    Score Script hinzugefuegt

---
+++ MainGame/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+    public int theScore = 0;
+
+
+	void Update () {
+
+	}
+    
+    void OnGUI()
+    {
+        //We display the game GUI from the playerscript
+        //It would be nicer to have a seperate script dedicated to the GUI though...
+        
+
+        GUILayout.Label("Score: " + theScore);
+    }    
+
+}
