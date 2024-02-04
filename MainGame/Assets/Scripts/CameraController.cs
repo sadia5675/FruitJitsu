538b6ea59cf357b231a02490e19bf6c9cc140f68
Huong Ly Nguyen <huongly.nguyen@student.hs-rm.de>
2024-02-04 13:47:17 +0100

    Kamera wechselung und collider von pico korrigiert

---
+++ MainGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 newCameraPosition;
+    public Vector3 newCameraRotation;
+    Vector3 basePos,baseRot;
+
+    Boolean isOnGameCamera = false;
+
+    void Start()
+    {
+        basePos=Camera.main.transform.position;
+        baseRot = Camera.main.transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            if(!isOnGameCamera) {
+                // Change camera position
+            Camera.main.transform.position = newCameraPosition;
+
+            // Change camera rotation
+            Camera.main.transform.eulerAngles = newCameraRotation;
+            isOnGameCamera = true;
+            } else {
+                 // Change camera position
+            Camera.main.transform.position = basePos;
+
+            // Change camera rotation
+            Camera.main.transform.eulerAngles = baseRot;
+            isOnGameCamera = false;
+            }
+            
+        }
+    }
+}
